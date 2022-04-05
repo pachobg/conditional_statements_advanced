40736b5eff7d32ff5335d9b200d668eea1903429
Пламен Ангелов <103038521+pachobg@users.noreply.github.com>
2022-04-05 21:17:51 +0300

    Add files via upload

---
+++ weekend_or_working_day.py
@@ -0,0 +1,18 @@
+week_day = input()
+
+if week_day == "Monday":
+    print("Working day")
+elif week_day == "Tuesday":
+    print("Working day")
+elif week_day == "Wednesday":
+    print("Working day")
+elif week_day == "Thursday":
+    print("Working day")
+elif week_day == "Friday":
+    print("Working day")
+elif week_day == "Saturday":
+    print("Weekend")
+elif week_day == "Sunday":
+    print("Weekend")
+else:
+    print("Error")

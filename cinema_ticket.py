08f38e1cce6097c34eae0277236d3069f5f835d6
Пламен Ангелов <103038521+pachobg@users.noreply.github.com>
2022-04-05 21:10:57 +0300

    Add files via upload

---
+++ cinema_ticket.py
@@ -0,0 +1,8 @@
+week_day = input()
+
+if week_day == "Monday" or week_day == "Tuesday" or week_day == "Friday":
+    print("12")
+elif week_day == "Wednesday" or week_day == "Thursday":
+    print("14")
+elif week_day == "Saturday" or week_day == "Sunday":
+    print("16")
